fd92c5a3606af3d5fcb956d7539b72394761ff40
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-10-19 16:43:29 -0400

    fix: add 2-task area of cercle

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -35,8 +35,14 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  if (!Number.isFinite(radius)) {
+    throw new Error('Not implemented');
+  }
+  if (radius < 0) {
+    throw new Error('radius must be >= 0');
+  }
+  return 2 * Math.PI * radius;
 }
 
 /**
