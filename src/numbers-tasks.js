16bc21c67f13dc7dce1f61fe2d010a129903a96a
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-10-23 19:53:04 -0400

    feat: add task 11 prine number

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -249,8 +249,25 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (typeof n !== 'number') {
+    throw new Error('Not implemented');
+  }
+  if (n <= 1) {
+    return false;
+  }
+  if (n === 2) {
+    return true;
+  }
+  if (n % 2 === 0) {
+    return false;
+  }
+  for (let i = 3; i * i <= n; i += 2) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
