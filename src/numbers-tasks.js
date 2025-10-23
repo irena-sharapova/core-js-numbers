083d9c5042cb697a83c01e4032518adc08662b0a
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-10-23 14:43:24 -0400

    feat: task 10 add power of ten

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -224,8 +224,12 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (typeof num !== 'number' && typeof pow !== 'number') {
+    throw new Error('Not implemented');
+  }
+  const result = 10 ** pow;
+  return Math.round(num / result) * result;
 }
 
 /**
